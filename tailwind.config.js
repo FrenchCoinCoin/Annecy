d73a04a87c008f3ae053cb4ada3d5cf9f066b3dc
marzomar <omar@frenchcoincoin.com>
2019-06-13 12:47:26 +0200

    Tailwindcss (header footer)
    Bug: text-color, bg-col, border-col and police font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      colors: {
+        red: '#f02d37',
+      }
+    },
+    extend: {
+        spacing: {
+      '72': '18rem',
+      '84': '21rem',
+      '96': '24rem',
+        }
+      }
+  }
