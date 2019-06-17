82c76c57c7d1fa6d38d661ac8f418eab22835c1f
marzomar <omar@frenchcoincoin.com>
2019-06-17 03:05:44 +0200

    Dim17/06 03h Ts les blocks
    Todo - Style (Police, marge, size,)
              - circle card,slider
              - relier article api wp
              - responsive
              -nettoyer,organiser code (component, partial)

--- nunjucks.config.js
+++ nunjucks.config.js
@@ -1,4 +1,67 @@
 module.exports = {
     root: "./src/html",
-    data: { name: "omar" },
+    data: {
+        name: "omar",
+        subtitle:"",
+        services: [
+            {
+                title: "Commander un acte d'état-civil",
+                categories: "Download"
+            },
+            {
+                title: "Camping municipal",
+                categories: "Book"
+            },
+            {
+                title: "Taxis : autorisation de stationnement",
+                categories: "Book"
+            },
+            {
+                title: "Location d'une parcelle de jardin",
+                categories: "Ask"
+            },
+            {
+                title: "Salles municipales",
+                categories: "Book"
+            },
+            {
+                title: "Boucles d'amarrage",
+                categories: "Ask"
+            },
+            {
+                title: "Marchés, foire, fête",
+                categories: "Ask"
+            },
+            {
+                title: "Meublés de tourisme, chambres d'hôte",
+                categories: "Ask"
+            },
+        ],
+        articles: [
+            {
+                date: "1 Janvier",
+                title: "Titre1",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            },
+            {
+                date: "2 Mars",
+                title: "Titre2",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            },
+            {
+                date: "3 Juin",
+                title: "Titre3",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            },
+            {
+                date: "4 Oct",
+                title: "Titre4",
+                content:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quo cupiditate excepturi quasi blanditiis sequi neque velit distinctio iusto laudantium fugiat explicabo quisquam nisi pariatur corporis deserunt, nobis rationelorem Lorem ipsum"
+
+            }
+        ]
+ },
 }
